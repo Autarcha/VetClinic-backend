b4494ec14e8187ea65990444505abafdf94a588d
patryk.graca <patry.graca@precisely.com>
2022-12-27 16:16:07 +0100

    Refacored code. Seperated ProfileController from userController

--- VetClinic-backend/Controllers/UserController.cs
+++ VetClinic-backend/Controllers/UserController.cs
@@ -43,15 +43,6 @@ namespace VetClinic_backend.Controllers
             return Ok(_mapper.Map<IEnumerable<UserDto>>(request));
         }
 
-        [HttpGet("Profile")]
-        [ProducesResponseType(200, Type = typeof(UserProfileDto))]
-        public async Task<ActionResult<UserProfileDto>> GetUserProfile()
-        {
-            var userId = int.Parse(User.Claims.First(x => x.Type == "id").Value);
-            var request = await _userRepository.GetUserById(userId);
-            return Ok(_mapper.Map<UserProfileDto>(request));
-        }
-
         [HttpPost("Register", Name = "RegisterUser")]
         [ProducesResponseType(200, Type = typeof(UserDto))]
         public async Task<ActionResult<UserDto>> RegisterUser(UserDetailsDto userRegisterData)
@@ -76,7 +67,7 @@ namespace VetClinic_backend.Controllers
             await _userRepository.AddUser(userRegisterMap);
             await _emailService.SendPasswordMail(userRegisterMap, userPassword);
             await _userRepository.SaveChangesAsync();
-            return Ok("Pomysnie utworzono użytkownika");
+            return Ok(_mapper.Map<UserDto>(userRegisterMap));
         }
 
         [AllowAnonymous]
@@ -97,88 +88,6 @@ namespace VetClinic_backend.Controllers
             HttpContext.Response.Headers.Add("Access-Control-Expose-Headers", "AuthToken");
             HttpContext.Response.Headers.Add("AuthToken", token);
             return Ok(_mapper.Map<UserDto>(result.Result));
-        }
-
-        [HttpPut("UpdateDetails")]
-        public async Task<ActionResult<UserDto>> UpdateUserDetails(UserDetailsDto request)
-        {
-            var userId = int.Parse(User.Claims.First(x => x.Type == "id").Value);
-
-            var user = await _userRepository.GetUserById(userId);
-
-            if (user is null)
-            {
-                ModelState.AddModelError("", "Nie znaleziono takiego użytkownika");
-                return StatusCode(404, ModelState);
-            }
-
-            if (!String.IsNullOrEmpty(request.Email))
-            {
-                var checkIfExist = await _userRepository.GetUserByEmail(request.Email);
-                if (checkIfExist is not null && user.Email != request.Email)
-                {
-                    return Problem("Zarejestrowano juz konto na ten mail");
-                }
-                user.Email = request.Email;
-            }
-
-            if (!String.IsNullOrEmpty(request.Name))
-            {
-                user.Name = request.Name;
-            }
-
-            if (!String.IsNullOrEmpty(request.Surname))
-            {
-                user.Surname = request.Surname;
-            }
-
-            if (!String.IsNullOrEmpty(request.PhoneNumber))
-            {
-                user.PhoneNumber = request.PhoneNumber;
-            }
-
-            var result = await _userRepository.UpdateUser(user);
-
-            return Ok(_mapper.Map<UserDto>(result));
-
-        }
-
-        [HttpPut("ChangePassword")]
-        public async Task<ActionResult<UserDto>> ChangePassword(UserChangePasswordDto request)
-        {
-            var userId = int.Parse(User.Claims.First(x => x.Type == "id").Value);
-
-            var user = await _userRepository.GetUserById(userId);
-
-            if (user is null)
-            {
-                ModelState.AddModelError("", "Nie znaleziono takiego użytkownika");
-                return StatusCode(404, ModelState);
-            }
-
-
-            if (!(String.IsNullOrEmpty(request.OldPassword) && String.IsNullOrEmpty(request.NewPassword)))
-            {
-                if (!BCrypt.Net.BCrypt.Verify(request.OldPassword, user.Password))
-                {
-                    ModelState.AddModelError("", "Błędne obecne hasło");
-                    return StatusCode(406, ModelState);
-                }
-
-
-                if (BCrypt.Net.BCrypt.Verify(request.NewPassword, user.Password)) { 
-
-                    ModelState.AddModelError("", "Nowe hasło nie może być takie samo jak poprzednie");
-                    return StatusCode(500, ModelState);
-                }
-
-                user.Password = BCrypt.Net.BCrypt.HashPassword(request.NewPassword);
-            }
-
-            var result = await _userRepository.UpdateUser(user);
-
-            return Ok(_mapper.Map<UserDto>(result));
-
         }
 
     }
